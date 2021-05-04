d08ae903ca312cdc979b337c04334f3d010f5144
Sameep <sameepsawant10@gmail.com>
2021-05-04 20:38:14 +0530

    Styled: Input Output

--- src/Components/Map/Inputmap.js
+++ src/Components/Map/Inputmap.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
  import {db} from '../../Firebase'
-
+import '../CSS/output.css'
 
 function Inputmap({change}) {
     const [getItems, setGetItems] = useState([])
@@ -20,13 +20,13 @@ function Inputmap({change}) {
 
 
     return (
-        <div>
+        <div className="inputmain">
            {
                getItems.length > 0?
                getItems.map(item =>(
-                   <div key={item.id}>
-                       <p>{item.Task}</p>
+                   <div key={item.id} className='itemoutput'>
                        <p>{item.Time}</p>
+                       <p>{item.Task}</p>
                    </div>
                )):
                <div></div>
